cfb2f08e9d610c8b13174478fd7dc4d16cd6e207
Ritesh Kumar <riteshpeepal@gmail.com>
2025-06-28 21:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello, World!");
+
+function ritesh(){
+    console.log("This is a function named Ritesh.");
+}
+ritesh();
+
+function bring_brush(number_of_brush){
+    console.log("This is a function named Brush.");
+    console.log(`Hanji le main ${number_of_brush} brush, aur krr rha hoon kaaam.`)
+}
+bring_brush(6);
+
+function add_number(ek_number, do_number){
+    return ek_number + do_number;
+}
+add_number(100000,367889);
+
+console.log("The sum of the two numbers is: " + add_number(100000, 367889));
+
